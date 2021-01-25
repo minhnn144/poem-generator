93f0216f42c76ca00529878a5c9bc2e9a417d974
nguyenmao2101 <maonguyen199873@gmail.com>
2021-01-25 22:01:02 +0700

    add predict ui app

---
+++ server.py
@@ -0,0 +1,60 @@
+from flask.templating import render_template
+import torch
+import argparse
+from lib import data_utils, vnlp
+from lib.dictionary import Dictionary
+from model import PoemGeneratorLightning
+from flask import Flask, make_response, request
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("--config", default="/config.ini")
+parser.add_argument("--chkp", default="lightning_logs/version_102/checkpoints/epoch=58-step=58.ckpt")
+
+args = parser.parse_args()
+
+config = data_utils.load_config(args.config)
+
+embed_size = config.getint('DEFAULT', 'emb_size')
+word_size = config.getint('DEFAULT', 'word_size')
+hid_size = config.getint('DEFAULT', 'hid_size')
+seq_len = config.getint('DEFAULT', 'seq_len')
+
+chkp_path = args.chkp
+
+nlp = vnlp.VNlp('model/wiki.vi.bin')
+corpus = data_utils.unpickle_file("/data/vectorized/word_list.pkl")
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET"])
+def index():
+    return render_template("index.html")
+
+@app.route("/predict", methods=['POST'])
+def predict():
+    data = request.get_json()
+    inp_ = torch.FloatTensor(corpus.vectors[corpus.word2idx["<SOS>"]])
+    inp_ = inp_.reshape(1, 1, -1)
+
+    sentiments = data.get("keywords")
+    sent = torch.FloatTensor(nlp.combined_vector(sentiments))
+    sent = sent.reshape(1, -1)
+
+    model = PoemGeneratorLightning.load_from_checkpoint(chkp_path, strict=True, word_size=word_size, embed_size=embed_size, hid_size=hid_size, seq_len=seq_len)
+    model.freeze()
+
+    outputs = []
+    for i in range(seq_len):
+        out = model(inp_, sent)
+        out_ = out.squeeze().exp()
+        out_ = torch.multinomial(out_, 1)[0]
+        tok = corpus.idx2word[int(out_)]
+        outputs.append(tok)
+        inp_ = torch.FloatTensor(corpus.vectors[int(out_)])
+        inp_ = inp_.reshape(1, 1, -1)
+    print(len(outputs))
+    print(" ".join(outputs))
+    return make_response(data=outputs)
+
+app.run("0.0.0.0", 1998)
